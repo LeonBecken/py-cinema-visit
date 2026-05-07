7d59ac96a5de7a385679e5299a7c58ef57db0f9a
Dmytro <dmitryibezkrovnyi@gmail.com>
2026-05-07 22:32:31 +0200

    fixed many sintaxis mistakes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
@@ -16,5 +16,5 @@ def cinema_visit(
     for customer in customers:
         customer_object = Customer(customer["name"], customer["food"])
         customers_list.append(customer_object)
-        CinemaBar.sell_product(customer_object)
+        CinemaBar.sell_product(customer["food"], customer_object)
     hall.movie_session(movie, customers_list, cleaner_object)
